bea451d70900ccb477e6aab704ac243ed4ce4945
Roshan Padaki <roshanpadaki@gmail.com>
2018-02-09 18:03:36 -0500

    Fixing things

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ var url = new URL(url_string);
 var concert_id = url.searchParams.get("concert_id");
 var pathReference = storage.ref('json/' + concert_id + '.json');
 var jsonURL = "";
-var jsonObject;
 pathReference.child('json/' + concert_id + '.json').getDownloadURL().then(
 function(url) {
 	jsonURL = url;
@@ -23,20 +22,21 @@ xmlhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 		var myObj = JSON.parse(this.responseText);
 		jsonObject = myObj;
+		xmlhttp.open("GET", jsonURL, true);
+		xmlhttp.send();
+		document.getElementById("title").innerHTML = jsonObject["name"];
+		var dateFormat = require('dateformat');
+		//document.getElementById("date").innerHTML = dateFormat(new Date(jsonObject["date"]), "dddd, mmmm dS, yyyy);
+		document.getElementById("venue").innerHTML = jsonObject["venue"];
+		document.getElementById("conductor").innerHTML = jsonObject["conductor"];
+		document.getElementById("performers").innerHTML = ", ".join(jsonObject["performers"]);
+		document.getElementById("concert-blurb").innerHTML = jsonObject["concert-blurb"];
+		document.getElementById("piece-blurb").innerHTML = jsonObject["piece-blurb"];
+		var programPathReference = storage.ref(jsonObject["program-location"]);
+		programPathReference.child(jsonObject["program-location"]).getDownloadURL().then(
+		function(url) {
+			document.getElementById("pdf-viewer").src = "http://docs.google.com/gview?url=" + url + "&embedded=true";
+		});
 	}
 };
-xmlhttp.open("GET", jsonURL, true);
-xmlhttp.send();
-document.getElementById("title").innerHTML = jsonObject["name"];
-var dateFormat = require('dateformat');
-//document.getElementById("date").innerHTML = dateFormat(new Date(jsonObject["date"]), "dddd, mmmm dS, yyyy);
-document.getElementById("venue").innerHTML = jsonObject["venue"];
-document.getElementById("conductor").innerHTML = jsonObject["conductor"];
-document.getElementById("performers").innerHTML = ", ".join(jsonObject["performers"]);
-document.getElementById("concert-blurb").innerHTML = jsonObject["concert-blurb"];
-document.getElementById("piece-blurb").innerHTML = jsonObject["piece-blurb"];
-var programPathReference = storage.ref(jsonObject["program-location"]);
-programPathReference.child(jsonObject["program-location"]).getDownloadURL().then(
-function(url) {
-	document.getElementById("pdf-viewer").src = "http://docs.google.com/gview?url=" + url + "&embedded=true";
-});
+

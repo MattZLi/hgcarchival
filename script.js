f646007a48508b72b4b88bf4a1e40519256df755
Roshan Padaki <roshanpadaki@gmail.com>
2018-02-10 02:54:21 -0500

    works now

--- script.js
+++ script.js
@@ -15,8 +15,7 @@ function(url) {
 			console.log(myObj);
 			jsonObject = myObj;
 			document.getElementById("title").innerHTML = jsonObject["name"];
-			var dateFormat = require('dateformat');
-			//document.getElementById("date").innerHTML = dateFormat(new Date(jsonObject["date"]), "dddd, mmmm dS, yyyy);
+			document.getElementById("date").innerHTML = new Date(jsonObject["date"]).toString("dddd, MMMM dS, yyyy");
 			document.getElementById("venue").innerHTML = jsonObject["venue"];
 			document.getElementById("conductor").innerHTML = jsonObject["conductor"];
 			document.getElementById("performers").innerHTML = ", ".join(jsonObject["performers"]);
